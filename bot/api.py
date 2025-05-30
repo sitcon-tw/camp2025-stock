da0876d348254b9418e3b429be3f16879ebe80ce
wolf-yuan <wolf@wolf-yuan.dev>
2025-05-30 23:36:37 +0800

    feat: add some logging to healthz

--- bot/api.py
+++ bot/api.py
@@ -65,11 +65,13 @@ async def broadcast_all(request: Request):
 async def healthz():
     try:
         await bot.bot.get_me()
+        logger.info("Healthz endpoint checked successfully.")
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={"ok": True}
         )
     except Exception as e:
+        logger.error(f"Error checking healthz: {e}")
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"ok": False}
