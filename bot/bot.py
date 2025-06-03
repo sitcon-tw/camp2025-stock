70b706b22d67c576f1baf7219b835f2ef1329a27
wolf-yuan <wolf@wolf-yuan.dev>
2025-06-03 13:40:04 +0800

    feat: add welcome message

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,5 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, Application, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, Application, ContextTypes, CommandHandler, ChatMemberHandler
 from telegram.constants import ParseMode
 from dotenv import load_dotenv
 from utils.logger import setup_logger
@@ -54,15 +54,43 @@ async def register(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
         f"""
         😸 喵嗚，{username}，原來你就是 *王小明* 啊！
-        
+
 很高興可以在 *SITCON Camp 2025* 看到你，希望你可以在這裡交到好多好多好朋友 😺
 我叫做喵券機，顧名思義就是拿來買股票券的機器人，你可以跟我買股票喵！
 
 *想現在就試試看嗎？*點一下底下的按鈕，開啟_*喵券機股票交易頁面吧！*_
         """, parse_mode=ParseMode.MARKDOWN_V2, reply_markup=InlineKeyboardMarkup(buttons))
 
+async def welcome_member(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_member_update = update.chat_member
+
+    old_member_status = chat_member_update.old_chat_member.status if chat_member_update.old_chat_member else None
+    new_member_status = chat_member_update.new_chat_member.status if chat_member_update.new_chat_member else None
+
+    if (old_member_status not in [ChatMember.MEMBER, ChatMember.OWNER, ChatMember.ADMINISTRATOR] and
+        new_member_status in [ChatMember.MEMBER, ChatMember.OWNER, ChatMember.ADMINISTRATOR]):
+
+        new_member = chat_member_update.new_chat_member.user
+        chat = update.effective_chat
+
+        if new_member and chat:
+            await context.bot.send_message(
+                chat_id=chat.id,
+                text=f"""
+                👋 你好 *{new_member.full_name}* ！
+歡迎加入 SITCON Camp 的小隊群組！我是喵券機，你在這個營隊中一直會看到我哦！
+
+這個群組是小隊 __*3*__ 的，請不要走錯地方囉
+如果你是這個小隊的，請在你的 email 裡面找一找一個 *註冊碼*，並在這個聊天室輸入 `/register 註冊碼` 來註冊
+>例如你的註冊碼是 `1234567890`，就要在這個小隊的頻道裡面輸入 `/register 1234567890`
+                """,
+                parse_mode=ParseMode.MARKDOWN_V2
+            )
+            logger.info(f"{new_member.username} joined chat {chat.id}")
+
 bot = ApplicationBuilder().token(BOT_TOKEN).build()
 
+bot.add_handler(ChatMemberHandler(welcome_member, ChatMemberHandler.CHAT_MEMBER))
 bot.add_handler(CommandHandler("start", start))
 bot.add_handler(CommandHandler("register", register))
 
