66e7d69df371c27e3f00fecb3891f74c4d5ccbdb
wolf-yuan <wolf@wolf-yuan.dev>
2025-06-06 19:10:13 +0800

    fix: port read problem

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=PORT,
+        port=int(PORT),
         reload=True,
         log_level="info"
     )
