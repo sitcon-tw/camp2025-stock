1e80360213f83875e9d30425d07ae0fa1956ae30
wolf-yuan <wolf@wolf-yuan.dev>
2025-06-07 01:48:16 +0800

    feat: broadcast route

--- bot/api/routes/broadcast.py
+++ bot/api/routes/broadcast.py
@@ -22,20 +22,6 @@ class BroadcastSelective(Broadcast):
 @router.post("/bot/broadcast/")
 async def broadcast(request: BroadcastSelective):
     logger.info("[FastAPI] Selective broadcast endpoint hit.")
-    for channel in request.channel:
-        try:
-            await bot.bot.send_message(channel, f"""
-📢 *{request.title}*
-
-{request.message}
-""", parse_mode=ParseMode.MARKDOWN_V2)
-        except Exception as e:
-            logger.error(f"Error broadcasting message: {e}")
-    return {"ok": True}
-
-@router.post("/bot/broadcast/all")
-async def broadcast_all(request: Broadcast):
-    logger.info("[FastAPI] Broadcast endpoint hit.")
     for channel in BROADCAST_CHANNELS:
         try:
             await bot.bot.send_message(channel, f"""
