3696111454a9d0587b2acfb5556554e83e24566c
Elvis Mao <info@elvismao.com>
2025-06-08 13:52:49 +0800

    favicon

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -52,6 +52,7 @@ export default function RootLayout({ children }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="theme-color" content="#0f203e" />
+                <link rel="icon" href="/icons/favicon.png" type="image/x-icon" sizes="16x16"></link>
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <meta name="apple-mobile-web-app-title" content="SITCON Camp 2025 點數系統" />
