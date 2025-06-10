1afb37fddcdeea789d9edc02e3515bc3fca97ee5
陳德生 <111033412+KoukeNeko@users.noreply.github.com>
2025-06-10 16:16:29 +0800

    refactor: enhance navbar styling for improved visual clarity
    - Update navbar background and border styles to incorporate transparency and backdrop blur for a modern look.
    - Adjust shadow properties to enhance depth and visual separation from the background.

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -12,8 +12,8 @@ export default function Navbar() {
     return (
         <div
             className="fixed bottom-4 left-1/2 transform -translate-x-1/2 max-w-[calc(100%-2rem)] gap-12
-                        bg-[#0f203e] py-4 px-10 flex items-center border-2 border-[#4f6f97] 
-                         z-40 rounded-full shadow-lg"
+                        bg-[#0f203e]/20 backdrop-blur-md py-4 px-10 flex items-center border-2 border-[#4f6f97]/20 
+                         z-40 rounded-full shadow-lg shadow-black/40"
         >
             {" "}
             <Link href="/" className={getIconColor("/")}>
