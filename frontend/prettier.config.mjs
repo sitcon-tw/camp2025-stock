4a6a5695c450f50af76cbd78e4adffe3b622190b
wolf-yuan <wolf@wolf-yuan.dev>
2025-06-14 23:37:10 +0800

    chore: we really need a formatter and some coding style standard

---
+++ frontend/prettier.config.mjs
@@ -0,0 +1,18 @@
+export default {
+    printWidth: 70,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    bracketSpacing: true,
+    arrowParens: "always",
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: "never",
+    plugins: [
+        "prettier-plugin-organize-imports",
+        "prettier-plugin-tailwindcss",
+    ],
+    htmlWhitespaceSensitivity: "strict",
+};
