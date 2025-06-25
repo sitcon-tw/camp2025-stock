d143a76788017a83973df51082f265a647126dca
wolf-yuan <wolf@wolf-yuan.dev>
2025-06-25 21:41:19 +0800

    chore: reformat code with new string processing mode

--- bot/api/routes/broadcast.py
+++ bot/api/routes/broadcast.py
@@ -1,13 +1,11 @@
 from os import environ
 from typing import Annotated
 
+from bot.instance import bot
 from fastapi import APIRouter, status, Header
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from telegram.constants import ParseMode
 from telegram.helpers import escape_markdown
-
-from bot.instance import bot
 from utils.logger import setup_logger
 
 router = APIRouter()
@@ -15,9 +13,11 @@ logger = setup_logger(__name__)
 BROADCAST_CHANNELS = environ.get("BROADCAST_CHANNELS").split(",")
 BACKEND_TOKEN = environ.get("BACKEND_TOKEN")
 
+
 class Broadcast(BaseModel):
     title: str
     message: str
+
 
 @router.post("/bot/broadcast/")
 async def broadcast(request: Broadcast, token: Annotated[str | None, Header()] = None):
@@ -36,11 +36,12 @@ async def broadcast(request: Broadcast, token: Annotated[str | None, Header()] =
     logger.info("[FastAPI] Selective broadcast endpoint hit.")
     for channel in BROADCAST_CHANNELS:
         try:
-            await bot.bot.send_message(f"-{channel}", f"""
-📢 *{escape_markdown(request.title)}*
-
-{escape_markdown(request.message)}
-""", parse_mode=ParseMode.MARKDOWN_V2)
+            await bot.bot.send_message(
+                f"-{channel}",
+                f"""📢 *{escape_markdown(request.title)}*"""
+                f"""{escape_markdown(request.message)}"""
+                f""", parse_mode=ParseMode.MARKDOWN_V2)"""
+            )
         except Exception as e:
             logger.error(f"Error broadcasting message: {e}")
     return {"ok": True}
