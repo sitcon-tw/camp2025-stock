2b2274cce6c9194801e773198e6a876f66cbc9ff
wolf-yuan <wolf@wolf-yuan.dev>
2025-07-05 20:29:18 +0800

    fix: admin login error

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -32,7 +32,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=settings.CAMP_JWT_EXPIRE_MINUTES)
+        expire = datetime.utcnow() + timedelta(minutes=int(settings.CAMP_JWT_EXPIRE_MINUTES))
 
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
